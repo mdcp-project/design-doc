56b0993761b53034fa1438759433aadf2b93dbae
Anton Rodionov <arodionov18@gmail.com>
2021-03-27 16:44:31 +0300

    Update auth service

--- src/auth/main.py
+++ src/auth/main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 import uvicorn
 import secrets
 import utils
 import datetime
+
+from db import SessionLocal, engine
+import models, schemas, utils
+
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
 
 class Credentials(BaseModel):
     email: str
@@ -14,30 +21,42 @@ class Token(BaseModel):
 
 app = FastAPI()
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get('/')
 def read_root():
     return {"Hello": "World"}
 
-@app.post('/signup/credentials')
-def signup(credentials: Credentials):
-    pass
+@app.post('/signup')
+def signup(credentials: Credentials, db: Session = Depends(get_db)):
+    if get_user_by_email(db, credentials.email):
+        raise HTTPException(402, detail="User with that email already exists")
+    user = create_user(db, schemas.UserCreate(email=credentials.email, password=credentials.password))
+    return {"result_code": 200, "user_id": user.id, "user_email": user.email}
 
-@app.get('/signin/credentials')
-def signin(credentials: Credentials):
-    pass
+@app.get('/signin')
+def signin(credentials: Credentials, db: Session = Depends(get_db)):
+    print(db, flush=True)
+    at, rt, session = create_session(db, schemas.UserAuthenticate(email=credentials.email, password=credentials.password))
+    if at is None:
+        raise HTTPException(403, detail="Wrong email/password")
+    return {"access_token": at, "refresh_token": rt, "result_code": 200}
 
 @app.get('/validate/token')
 def validate(token: Token):
     pass
 
-from sqlalchemy.orm import Session
-import models, schemas
-
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(user=user.email)
+    db_user = models.User(email=user.email)
     db_user.set_password(user.password)
     db.add(db_user)
     db.commit()
@@ -46,31 +65,33 @@ def create_user(db: Session, user: schemas.UserCreate):
 
 def check_email_password(db: Session, user: schemas.UserAuthenticate):
     db_user = get_user_by_email(db, email=user.email)
-    if user is None:
+    if db_user is None:
         return False
     return db_user.check_password(user.password)
 
 def create_tokens(email, session: models.Session, time=datetime.datetime.utcnow()):
     refresh_token = secrets.token_hex(64)
-    access_token = utils.create_access_token({'email': email, 'session': session.id})
+    access_token = utils.create_access_token(data={'email': email, 'session': session.id})
     return access_token, refresh_token
 
 def create_session(db: Session, user: schemas.UserAuthenticate):
     if not check_email_password(db, user):
-        return None
+        return None, None, None
     
     db_user = get_user_by_email(db, user.email)
-    session = models.Session(userId=db_user.id)
 
-    db.add(session)
-    db.commit()
-    db.refresh(session)
+    session = db.query(models.Session).filter(models.Session.userId == db_user.id).first()
+    if session is None:
+        session = models.Session(userId=db_user.id)
+        db.add(session)
+        db.commit()
+        db.refresh(session)
 
     now = datetime.datetime.utcnow()
     access_token, refresh_token = create_tokens(db_user.email, session, now)
     session.refreshToken = refresh_token
-    session.refreshTokenExpirationDate = now + 15
-    db.add(session)
+    session.refreshTokenExpirationDate = now + datetime.timedelta(minutes = 15)
+    #db.add(session)
     db.commit()
     db.refresh(session)
     return access_token, refresh_token, session
--- src/auth/utils.py
+++ src/auth/utils.py
@@ -9,7 +9,7 @@ def create_access_token(*, data: dict, expires_delta: timedelta = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=5)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
     return encoded_jwt
